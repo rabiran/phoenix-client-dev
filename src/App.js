47b95ad33a50c772694d58f7abe3dcad88515df6
Grisha <codpure@gmail.com>
2019-12-15 14:38:57 +0200

    manage page and router (#2)

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import Header from './components/Header';
 import { theme } from './theme.js';
 import { ThemeProvider } from '@material-ui/core/styles';
 import RTL from './components/RTL';
-// Configure JSS
+import ManagePage from './components/ManagePage/ManagePage';
+import LandingPage from './components/LandingPage/LandingPage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
       <RTL>
         <ThemeProvider theme={theme}>
             <Header/>
+            <Router>
+              <Switch>
+                  <Route exact path='/' component={LandingPage} />
+                  <Route path='/managepage' component={ManagePage} />
+              </Switch>
+            </Router>
         </ThemeProvider>
       </RTL>
   );
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../../App.css';
+
+export default function LandingPage() {
+  return (
+      <p>Landing Page</p>
+  );
+}
+
