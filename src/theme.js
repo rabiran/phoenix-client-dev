a004fd34d75350d74a4ef789b3ffba3e10d85377
elad biran <eladex@gmail.com>
2020-01-28 12:21:18 +0200

    add selected theme color

--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,18 @@
 
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 import teal from '@material-ui/core/colors/teal';
 
-export const theme = createMuiTheme({
+
+export const theme = responsiveFontSizes(createMuiTheme({
     direction: 'rtl',
     palette: {
-      // text: {
-      //   primary: 'rgba(0,255,0,0.87)'
-      // },
+      action: {
+        selected: teal[400]
+      },
       primary: teal,
-      secondary: red, // Indigo is probably a good match with pink
+      secondary: red,
       type: 'light'
     },
-});
+}));
   
