18e3fb4ef6483872cee2a3e1ab224f20eca546db
elad biran <eladex@gmail.com>
2020-02-03 15:50:56 +0200

    add actions to group slice

--- src/features/groups/groupsSlice.js
+++ src/features/groups/groupsSlice.js
@@ -6,10 +6,20 @@ const groupsSlice = createSlice({
   name: 'groups',
   initialState: mockIntialState.groups,
   reducers: {
-
+    fetchChildrenSuccess(state, action) {
+      const { parentId, children } = action.payload;
+      const newChildrenById = children.map(c => ({
+        [c.id]: c,
+      }));
+      const newChildrenIds = children.map(c => c.id);
+      Object.assign(state.byId, ...newChildrenById);
+      state.byId[parentId].children = newChildrenIds;
+    },
+    fetchChildrenRequest(){}
   },
 });
 
+// selectors
 const getGroups = (state) => state.groups.byId;
 const getRootGroupsIds = (state) => state.groups.rootGroupsIds;
 export const selectRootGroups = createSelector(
@@ -17,6 +27,19 @@ export const selectRootGroups = createSelector(
   (groupsByid, rootIds) => rootIds.map(id => groupsByid[id])
 );
 export const selectGroupByid = (state, id) => getGroups(state)[id];
- 
 
+
+// Actions
+export const { 
+  /**
+   * @param id the id of the group to fetch children for.
+   */
+  fetchChildrenRequest, 
+  /**
+   * payload = { parentId: string, children: [group] }
+   * @param payload.parentId the id of the parent group
+   * @param payload.children the fetched children group objects
+   */
+  fetchChildrenSuccess 
+} = groupsSlice.actions;
 export default groupsSlice.reducer;
