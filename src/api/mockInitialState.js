479f034c28dfa5321cd7d2c2621d9446b193666a
elad biran <eladex@gmail.com>
2020-02-20 09:05:25 +0200

    clean group slice initial state

--- src/api/mockInitialState.js
+++ src/api/mockInitialState.js
@@ -1,36 +1,8 @@
 export default {
   groups: {
-    rootGroupsIds: ['5dc3ee650d65cff313e2a944'],
-    byId: {
-      "5dc3ee650d65cff313e2a944": {
-        id: '5dc3ee650d65cff313e2a944',
-        name: 'wallmart',
-      }
-    }
+    rootGroupsIds: [],
+    subtreeLoaded: {},
+    byId: {}
   }
-  // groups: {
-  //   rootGroupsIds: ['1', '4'],
-  //   byId: {
-  //     "1": {
-  //       id: '1',
-  //       name: 'אחד',
-  //       children: ['2','3']
-  //     },
-  //     "2": {
-  //       id: '2',
-  //       name: 'שתיים',
-  //       isAleaf: true
-  //     },
-  //     "3": {
-  //       id: '3',
-  //       name: 'שלוש',
-  //       isAleaf: true
-  //     },
-  //     "4": {
-  //       id: '4',
-  //       name: 'ארבע',
-  //       // children: ['5']
-  //     },
-  //   }
-  // }
 };
+
