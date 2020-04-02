0668cdd8fc7d0984ccccd42624192fe578b276d0
eladex <eladex@gmail.com>
2020-04-02 12:37:46 +0300

    person API

---
+++ src/api/persons/api.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { fetchGroupMembers } from '../groups/api'
+const BASE_URL = 'api/persons';
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+});
+
+const fetchById = async id => {
+  const res = (await instance.get(`/${id}`)).data;
+  return personFromApiResponse(res);
+}
+
+const fetchByGroupId = async id => {
+  const res = await fetchGroupMembers(id);
+  return res.map(personFromApiResponse);
+};
+
+const personFromApiResponse = person => {
+  const { _id, ...rest } = person;
+  return rest;
+};
+
+export {
+  fetchById,
+  fetchByGroupId
+};
+
+export default {
+  fetchById,
+  fetchByGroupId
+}
---
+++ src/api/persons/index.js
@@ -0,0 +1,8 @@
+import api from './api';
+
+export const {
+  fetchByGroupId,
+  fetchById,
+} = api;
+
+export default api;
