0f747c515f184d33917d7603a1162901729417f9
eladex <eladex@gmail.com>
2020-04-02 12:39:41 +0300

    person saga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,10 @@
 import { all } from 'redux-saga/effects';
 import groupsRoot from './features/groups/groupSaga';
+import personsRoot from './features/persons/personSaga';
 
 export default function* rootSaga() {
   yield all([
-    groupsRoot()
+    groupsRoot(),
+    personsRoot()
   ]);
 }
