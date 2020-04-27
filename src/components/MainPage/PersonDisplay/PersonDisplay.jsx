3c223b05c7ca0efbc999b6338916129a0923200d
elad biran <eladex@gmail.com>
2020-04-27 16:33:24 +0300

    filter in personDisplay

--- src/components/MainPage/PersonDisplay/PersonDisplay.jsx
+++ src/components/MainPage/PersonDisplay/PersonDisplay.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import Divider from '@material-ui/core/Divider';
+import Input from '@material-ui/core/Input';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
 import Typography from '@material-ui/core/Typography';
 import { useSelector, useDispatch } from 'react-redux';
 import Spinner from 'components/common/Loading/Spinner'
@@ -21,20 +25,82 @@ const styles = makeStyles({
   },
 });
 
+const inputStyles= makeStyles(theme => ({
+  root: {
+    backgroundColor: 'white',
+    borderRadius: '15px',
+    '&$focused $notchedOutline, &:hover$focused $notchedOutline': {
+      borderWidth: '1px',
+      borderColor: theme.palette.primary.main,
+    },
+    '&:hover $notchedOutline': {
+      borderColor: '#e0e0e0',
+    }
+  },
+  input: {
+    padding: '9px',
+  },
+  notchedOutline: {
+    borderColor: '#e0e0e0',
+    // borderColor: 'red',
+  },
+  focused: {}
+}));
+
+const labelContainerStyles = makeStyles({
+  root: {
+    width: '13%', 
+    minWidth: '130px'
+  }
+});
 
 const PersonDisplay = props => {
   const {
     groupId,
   } = props;
+
+  
+  // styles
   const classes = styles();
+  const inputClasses = inputStyles();
+  const labelContainerClasses = labelContainerStyles();
+  
+  // 
+  const [filterTerm, setFilter] = useState('a');
+  useEffect(()=> {
+    console.log('efectttttttttt');
+    console.log('filter:', filterTerm);
+  });
   const persons = useSelector(state => selectPersonsByGroupId(state, groupId)) || [];
+
+  
+  const ff = persons.filter(p => p.fullName.startsWith(filterTerm));
   const loading = useSelector(state => selectIsLoadingByGroupId(state, groupId));
   const group = useSelector(state => selectGroupByid(state, groupId));
   const groupName = group ? group.name : '...';
 
+
+
+  
+
   return (
     <div className={classes.root}>
-      <Typography variant="h6">{groupName}</Typography>
+      <Grid container spacing={2} alignItems="flex-end" style={{marginBottom:'3px'}}> 
+        <Grid item className={labelContainerClasses.root}><Typography display="inline" variant="h4">{groupName}</Typography></Grid>
+        <Grid item>
+          <OutlinedInput
+            onChange={e => setFilter(e.target.value)}
+            value={filterTerm}
+            // classes={{
+            //   input: inputClasses.input,
+            //   root: inputClasses.root,
+            //   notchedOutline: inputClasses.notchedOutline,
+            //   focused: inputClasses.focused
+            // }}
+            classes={{...inputClasses}}
+          />
+        </Grid>
+      </Grid>
       <Divider/>
       <div className={classes.content}>
         { loading ? <Spinner/> : <PersonGrid persons={persons}/>}
