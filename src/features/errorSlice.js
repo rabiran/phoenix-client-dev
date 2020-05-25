bdbdd0f267cb3719f96ec86b6f63ce6792510815
elad biran <eladex@gmail.com>
2020-05-25 11:09:59 +0300

    error slice: reducer, action & selectors

---
+++ src/features/errorSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, createAction } from '@reduxjs/toolkit'
+
+export const SLICE_NANE = 'errors';
+
+const initialState = {
+  error: null,
+  isOpen: false
+}
+
+// actions
+export const hideError = createAction(`${SLICE_NANE}/hideError`);
+export const setError = createAction(`${SLICE_NANE}/setError`, error => ({
+  payload: {},
+  error,
+}));
+
+// selectors
+export const selectIsOpen = state => state[SLICE_NANE].isOpen;
+export const selectErrorObj = state => state[SLICE_NANE].error;
+
+export default function errorReducer(state = initialState, action) {
+  const { error } = action;
+  if (error) {
+    return { error, isOpen: true };
+  } else if (action.type === hideError.type) {
+    return { error: null, isOpen: false };
+  }
+  return state;
+}
--- src/features/rootReducer.js
+++ src/features/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import personsReducer from './persons/personsSlice';
 import groupsReducer from './groups/groupsSlice';
+import errorReducer, { SLICE_NANE as errors} from './errorSlice';
 
 export default combineReducers({
   groups: groupsReducer,
   persons: personsReducer,
+  [errors]: errorReducer,
 });
