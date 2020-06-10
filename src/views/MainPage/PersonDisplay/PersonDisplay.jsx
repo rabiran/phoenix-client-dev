99431f732814d777815ea238b3685402b2221311
elad biran <eladex@gmail.com>
2020-06-10 09:38:30 +0300

    refactor(PersonDisplay): separate SearchInput
    move SearchInput component to a separate file

--- src/views/MainPage/PersonDisplay/PersonDisplay.jsx
+++ src/views/MainPage/PersonDisplay/PersonDisplay.jsx
@@ -13,6 +13,7 @@ import { selectGroupByid } from 'features/groups/groupsSlice';
 import PersonGrid from 'components/persons/personGrid';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
+import SearchInput from './SearchInput';
 
 import faker from 'faker/locale/en';
 const fakePersons =  [...Array(1000).keys()].map(i => ({id: i, fullName: `${faker.name.findName().toLowerCase()}`}));
@@ -21,39 +22,12 @@ const fakePersons2 =  [...Array(99).keys()].map(i => ({id: i, fullName: `elad${i
 const styles = makeStyles({
   root: {
     overflow: 'hidden',
-    // minHeight: '500px',
-    // padding: '15px'
-    width: '80%'
+    // width: '80%'
   },
   content: {
     height:'75%',
-    // overflowY: 'auto',
-    // overflowX: 'hidden',
-    // width: '80%'
   },
 });
-
-const inputStyles = makeStyles(theme => ({
-  root: {
-    backgroundColor: 'white',
-    borderRadius: '15px',
-    '&$focused $notchedOutline, &:hover$focused $notchedOutline': {
-      borderWidth: '1px',
-      borderColor: theme.palette.primary.main,
-    },
-    '&:hover $notchedOutline': {
-      borderColor: '#e0e0e0',
-    }
-  },
-  input: {
-    padding: '9px',
-  },
-  notchedOutline: {
-    borderColor: '#e0e0e0',
-    // borderColor: 'red',
-  },
-  focused: {}
-}));
 
 const headerStyles = makeStyles({
   root: {
@@ -82,23 +56,24 @@ const PersonDisplay = ({ groupId }) => {
   // styles
   const classes = styles();
   const headerClasses = headerStyles();  
-  // 
+  // debounce the filterTerm update
   const [filterTerm, setFilter] = useState('');
   const setFilterDebounced = useRef(_.debounce(setFilter));
+  const filterInputChange = useCallback(value => {
+    setFilterDebounced.current(value);
+  }, [setFilterDebounced]) 
   // const persons = (useSelector(state => selectPersonsByGroupId(state, groupId)) || [])
   const persons = fakePersons
     .filter(p => p.fullName.startsWith(filterTerm));
   const loading = useSelector(state => selectIsLoadingByGroupId(state, groupId));
   const group = useSelector(state => selectGroupByid(state, groupId));
-  const groupName = group ? group.name : '...';
-
-  const filterInputChange = useCallback(value => {
-    setFilterDebounced.current(value);
-  }, [setFilterDebounced]) 
+  // const groupName = group ? group.name : '...';
+  const groupName = group ? group.hierarchy.concat([group.name]).join('/') : '...';
 
   return (<>
     <Grid container spacing={2} alignItems="flex-end" justify='space-between' className={headerClasses.root}> 
       <Grid item className={headerClasses.groupName}>
+        {/* { group ? group.hierarchy.map()} */}
         <Typography display="inline" variant="h4">{groupName}</Typography>
         <Typography className={headerClasses.memberCount} display="inline" variant="body1">({persons.length})</Typography>
       </Grid>
@@ -106,6 +81,8 @@ const PersonDisplay = ({ groupId }) => {
         <SearchInput
           onValueChange={filterInputChange}
           resetOn={groupId}
+          validator={inputValidate}
+          placeholder='חפש אנשים בקבוצה'
         />
       </Grid>
     </Grid>
@@ -120,37 +97,6 @@ const PersonDisplay = ({ groupId }) => {
 
 PersonDisplay.propTypes = {
   groupId: PropTypes.string.isRequired,
-}
-
-const SearchInput = ({ onValueChange, resetOn }) => {
-  const [value, setValue] = useState('');
-  const onChange = e => {
-    const val = e.target.value;
-    if (inputValidate(val)){
-      setValue(val);
-      onValueChange(val);
-    }
-  };
-  useEffect(() => {
-    setValue('');
-    onValueChange('')
-  }, [resetOn, onValueChange]);
-  
-  const classes = inputStyles();
-
-  return (
-    <OutlinedInput
-      fullWidth
-      onChange={onChange}
-      value={value}
-      placeholder='חפש אנשים בקבוצה'
-      endAdornment={
-        <InputAdornment>
-          <SearchIcon/>
-        </InputAdornment>
-      }
-      classes={{...classes}}/>
-  );
 }
 
 export default PersonDisplay;
