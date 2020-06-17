462d5784dca2384522f18eb9b630e94757cd6f86
grisha <codpure@gmail.com>
2020-06-17 14:40:54 +0300

    what

--- src/api/groups/mockApi.js
+++ src/api/groups/mockApi.js
@@ -2,15 +2,15 @@ import rootGroup from './rootGroup';
 
 const timeout = 500;
 
-// let nextId = 1;
+let nextId = 1;
 
 const resolveAfter = (data, timeoutMs = timeout) => new Promise((resolve, reject) => {
   setTimeout(() => resolve(data), timeoutMs);
 });
 
-// const fetchGroupChildren = id => id === '4' ? 
-//   resolveAfter([{ id: '5', name: 'חמש', isAleaf: true }, {id: '6', name: 'שש', isAleaf: true}, {id: '4', name: 'ארבע', children: ['5','6']},]) 
-//   : resolveAfter([]);
+const fetchGroupChildren = id => id === '4' ? 
+  resolveAfter([{ id: '5', name: 'חמש', isAleaf: true }, {id: '6', name: 'שש', isAleaf: true}, {id: '4', name: 'ארבע', children: ['5','6']},]) 
+  : resolveAfter([]);
 
 const fetchSubtree = async (parentId) => {
   const pId = Number(parentId);
