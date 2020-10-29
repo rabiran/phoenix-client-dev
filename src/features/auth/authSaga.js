d0f90bd57b501c4242d159faf250c1fd81ae901a
eladex <eladex@gmail.com>
2020-10-30 00:00:51 +0200

    feature(auth): add mock login saga

--- src/features/auth/authSaga.js
+++ src/features/auth/authSaga.js
@@ -1,10 +1,12 @@
-import { put, call, takeEvery, all } from 'redux-saga/effects';
+import { put, delay } from 'redux-saga/effects';
 import { getUser } from 'api/auth';
 import { safeCall, safe } from 'utils/saga.helpers';
 import { loginSuccess, loginError } from './authSlice';
 
 
-export function* login() {
+const mockAuth = process.env.REACT_APP_MOCK_AUTH === 'true';
+
+function* _login() {
   const { result: user, error } = yield safeCall(getUser);
   if(!error) {
     yield put(loginSuccess({ user }))
@@ -13,4 +15,48 @@ export function* login() {
   }
   return { error }
 }
+
+function* fakeLogin(){
+  const user = {
+    "firstName": "נייקי",
+    "lastName": "אדידס",
+    "domainUsers": [
+    {
+    "dataSource": "dataSource1",
+    "uniqueID": "t23458789@jello.com",
+    "adfsUID": "t23458789@jellouid"
+    }
+    ],
+    "address": "רחוב הממתקים 34",
+    "clearance": "0",
+    "createdAt": "2020-02-26T15:01:06.870Z",
+    "currentUnit": "nitro unit",
+    "directGroup": "5e56858e4203fc40043591a5",
+    "entityType": "digimon",
+    "fullName": "נייקי אדידס",
+    "hierarchy": [
+    "כובעי הקש"
+    ],
+    "identityCard": "312571458",
+    "job": "רוצח",
+    "mail": "t23458789@jello.com",
+    "mobilePhone": [
+    "052-1234567"
+    ],
+    "phone": [
+    "026666998"
+    ],
+    "rank": "mega",
+    "responsibility": "none",
+    "status": "active",
+    "updatedAt": "2020-10-22T05:38:29.964Z",
+    "id": "5e5688324203fc40043591aa",
+    "dischargeDay": "2022-11-30T22:00:00.000Z"
+  }
+  yield delay(1000);
+  yield put(loginSuccess({ user }))
+}
+
+
+export const login = mockAuth ? fakeLogin : _login;
 
