1eae9c6bd5d15d3c3c1f77273e59d8048c59b065
elad biran <eladex@gmail.com>
2020-11-16 09:13:17 +0200

    feature(PersonDisplay): show profile dialog on item click

---
+++ src/views/MainPage/GroupMembersDisplay/PersonGrid.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import Grid from 'components/persons/personGrid';
+import PersonGridItem from 'components/persons/personGrid/PersonGridItem';
+import ProfileDialog from 'components/persons/personDetails/PersonProfileDialog';
+import { makeStyles } from '@material-ui/styles';
+
+
+const gridItemStyles = makeStyles(theme => ({
+  root: {
+    '&:hover': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    cursor: 'pointer'
+  }
+}));
+
+const PersonGrid = ({
+  persons,
+  itemWidth,
+  itemHeight,
+  spacing
+}) => {
+  const gridItemClasses = gridItemStyles();
+
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [clickedPerson, setClickedPerson] = useState({});
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  };
+  const handleGridItemClick = (e, itemData) => {
+    setClickedPerson(itemData);
+    setDialogOpen(true);
+  }
+
+  return (<>
+    <Grid
+      persons={persons}
+      itemWidth={itemWidth} 
+      itemHeight={itemHeight} 
+      spacing={spacing}
+      itemRenderer={({ itemData: person, style }) => {
+        return (
+          <PersonGridItem
+            classes={gridItemClasses}
+            label={person.fullName}
+            width={style.width}
+            style={style}
+            onClick={e => handleGridItemClick(e, person)}
+          />
+        )
+      }}
+    />
+    <ProfileDialog
+      open={dialogOpen}
+      onClose={handleDialogClose}
+      person={clickedPerson}
+    />
+  </>);
+}
+
+export default PersonGrid;
